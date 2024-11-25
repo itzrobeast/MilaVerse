76d93f917d95d6f6538baa3bd3f3e57f49382475
itzrobeast <robizzell@gmail.com>
2024-11-25 01:37:21 -0800

    add

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <link rel="icon" href="/favicon.ico" />
+      
         {/* Load Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
