35a65f70664f63d7cdc472a887f8ba694facc2f5
itzrobeast <robizzell@gmail.com>
2024-11-26 20:16:23 -0800

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,9 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-
 export async function apiFetch(endpoint, options = {}) {
   const url = `${BASE_URL}${endpoint}`;
-  const defaultHeaders = {
-    'Content-Type': 'application/json',
-  };
+  console.log('[DEBUG] Fetch URL:', url);
+  console.log('[DEBUG] Request Options:', options);
+
+  const defaultHeaders = { 'Content-Type': 'application/json' };
 
   try {
     const response = await fetch(url, {
