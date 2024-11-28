1605cfc11972655c910a347ad1263b744ac3f573
itzrobeast <robizzell@gmail.com>
2024-11-27 17:17:35 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="icon" href="/favicon.ico" />
-      
-        {/* Load Google Fonts */}
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
@@ -18,6 +16,23 @@ export default function Document() {
           defer
           crossOrigin="anonymous"
           src="https://connect.facebook.net/en_US/sdk.js"
+        ></script>
+
+        {/* Initialize Facebook SDK */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fbAsyncInit = function() {
+                FB.init({
+                  appId: '${process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}',
+                  cookie: true,
+                  xfbml: true,
+                  version: 'v16.0'
+                });
+                console.log('Facebook SDK initialized globally.');
+              };
+            `,
+          }}
         ></script>
       </Head>
       <body>
