383749dba8509258f11aa6b0824c331a97ab3143
itzrobeast <robizzell@gmail.com>
2024-12-08 18:45:56 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
       <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
@@ -5,12 +7,14 @@ export default function Home() {
         <p className="mt-4 text-lg text-gray-600">
           Automate your Instagram messaging and grow your business!
         </p>
-        <a
-          href="/auth/login"
-          className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-        >
-          Get Started
-        </a>
+        import Link from 'next/link';
+
+<Link href="/auth/login">
+  <a className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+    Get Started
+  </a>
+</Link>
+
       </div>
     );
   }
