3b914531ee2b8fc188ed688c08194cb7821823b6
itzrobeast <robizzell@gmail.com>
2024-12-08 18:44:06 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
           Automate your Instagram messaging and grow your business!
         </p>
         <a
-          href="/login"
+          href="/auth/login"
           className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
         >
           Get Started
