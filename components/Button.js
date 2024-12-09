13ad67c9d0325694359c27e8d4c20d455494466a
itzrobeast <robizzell@gmail.com>
2024-12-09 00:10:59 -0800

    Update Button.js

---
+++ components/Button.js
@@ -0,0 +1,7 @@
+export default function Button({ children, onClick }) {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
