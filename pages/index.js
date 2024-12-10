8af7d4b027bfe3fa1d14b9c7d54f783a07f87f86
itzrobeast <robizzell@gmail.com>
2024-12-09 17:28:05 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import Link from 'next/link';
 
 export default function Home() {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-5xl font-bold text-gray-800">Welcome to MilaVerse</h1>
-        <p className="mt-4 text-lg text-gray-600">
-          Automate your Instagram messaging and grow your business!
-        </p>
-        import Link from 'next/link';
-
-<Link href="/login">
-  <a className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-    Get Started
-  </a>
-</Link>
-
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-gray-800">Welcome to MilaVerse</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Automate your Instagram messaging and grow your business!
+      </p>
+      <Link href="/login">
+        <a className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+          Get Started
+        </a>
+      </Link>
+    </div>
+  );
+}
