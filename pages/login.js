e8be9f60f72a66d32cbb0f1e5deec008dc6ad2e9
itzrobeast <robizzell@gmail.com>
2024-12-11 16:46:43 -0800

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Cookies from 'js-cookie'; // Import the Cookies library
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
