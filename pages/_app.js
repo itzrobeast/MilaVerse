3702ae957e31ca90beaaee8bc55b7f3b8a82ef30
itzrobeast <robizzell@gmail.com>
2024-12-12 21:15:43 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { verifySession } from '../utils/auth';
 
+const PUBLIC_ROUTES = ['/', '/login']; // Routes that don't require authentication
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [user, setUser] = useState(null);
@@ -12,9 +14,13 @@ function MyApp({ Component, pageProps }) {
 
     const checkSession = async () => {
       const facebookAccessToken = localStorage.getItem('facebookAccessToken');
+
       if (!facebookAccessToken) {
-        console.log('[DEBUG] No Facebook access token found. Redirecting to login...');
-        if (router.pathname !== '/login') router.push('/login');
+        console.log('[DEBUG] No Facebook access token found.');
+        if (!PUBLIC_ROUTES.includes(router.pathname)) {
+          console.log('[DEBUG] Redirecting to login...');
+          if (isMounted) router.push('/login');
+        }
         if (isMounted) setLoading(false);
         return;
       }
@@ -24,7 +30,9 @@ function MyApp({ Component, pageProps }) {
 
       if (!userData) {
         console.error('[DEBUG] Invalid session. Redirecting to login...');
-        if (router.pathname !== '/login') router.push('/login');
+        if (!PUBLIC_ROUTES.includes(router.pathname)) {
+          if (isMounted) router.push('/login');
+        }
       } else {
         console.log('[DEBUG] Session verified successfully:', userData);
         if (isMounted) setUser(userData);
